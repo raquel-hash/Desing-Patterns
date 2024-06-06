c6815606772789cb93c778df534b57998c5a39e7
Raquel <quelcaraquel1@gmail.com>
2024-06-06 14:36:17 -0400

    Implementación de Singleton en JS

---
+++ creational/singleton.js
@@ -0,0 +1,29 @@
+/**
+ * 1. Make the constructor private
+ * 2. Create a static method who calls the private constructor
+ * and save the instance in a static variable 
+ */
+class Singleton {
+    static instance = undefined;
+    constructor(version) {
+        this.version = version;
+    }
+
+    static getInstance(version) {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton(version);
+        }
+        return Singleton.instance;
+    }
+}
+
+function appSingleton() {
+    const singleton1 = Singleton.getInstance("Version -1");
+    const singleton2 = Singleton.getInstance("Version -2");
+    const singleton3 = Singleton.getInstance("Version -3");
+
+    console.log(singleton1 === singleton2);
+    console.log(singleton1 === singleton3);
+}
+
+appSingleton();
